1a903613ae87f7270c27dc971a76ff78241a698e
Chetan Bhalerao <chetanbhalerao@Chetans-MacBook-Pro.local>
2022-11-30 18:27:14 +0530

    setup data and repo package

--- app/src/main/java/com/example/coroutinetutorial/data/model/SearchResponse.kt
+++ app/src/main/java/com/example/coroutinetutorial/data/model/SearchResponse.kt
@@ -1,3 +1,8 @@
 package com.example.coroutinetutorial.data.model
 
-data class SearchResponse()
+import com.google.gson.annotations.SerializedName
+
+data class SearchResponse(
+    @SerializedName("publishedDate")
+    val publishedDate: String
+)
---
+++ app/src/main/java/com/example/coroutinetutorial/data/repo/BookList.kt
@@ -0,0 +1,3 @@
+package com.example.coroutinetutorial.data.repo
+
+class BookList
